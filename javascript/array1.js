c3592d54d6e64b6e39a03e58518df8fd43e74169
chandana-2005-hub <nandyalachandana85@gmail.com>
2026-03-13 11:49:20 +0530

    Add files via upload

---
+++ javascript/array1.js
@@ -0,0 +1,11 @@
+let arr=[1,2,3,4,5];
+arr.pop(4)
+console.log(arr);
+let arr1=[1,2,3,4,5];
+arr1.shift(2);
+console.log(arr1);
+let arr2=[1,2,3,4,5];
+arr2.unshift(2);
+console.log(arr2);
+console.log(arr2.length);
+
