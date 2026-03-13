26df1d4d9564dad2dad7f9c1b7196e359e0df89e
chandana-2005-hub <nandyalachandana85@gmail.com>
2026-03-13 11:50:39 +0530

    Add files via upload

---
+++ javascript/string.js
@@ -0,0 +1,6 @@
+let str1="hello,my self chandana";
+let str2='hello,javascript';
+let str3='hello,universe';
+console.log(str1);
+console.log(str2);
+console.log(str3);                                                                                     
